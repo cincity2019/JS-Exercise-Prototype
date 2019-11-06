ddbf35dfc79fd79acfe52e83ca9c78ae5bf8ff2e
cincity2019 <cnkelly73@gmail.com>
2019-11-05 22:04:28 -0500

    questions

--- index.js
+++ index.js
@@ -9,16 +9,15 @@
 
 // EXAMPLE SOLUTION CODE:
 function Airplane(name) {
-  this.name = name;
-  this.isFlying = false;
+	this.name = name;
+	this.isFlying = false;
 }
-Airplane.prototype.takeOff = function () {
-  this.isFlying = true;
+Airplane.prototype.takeOff = function() {
+	this.isFlying = true;
 };
-Airplane.prototype.land = function () {
-  this.isFlying = false;
+Airplane.prototype.land = function() {
+	this.isFlying = false;
 };
-
 
 /*
 // 👇 COMPLETE YOUR WORK BELOW 👇
@@ -39,9 +38,22 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+	this.stomach = [];
 }
+Person.prototype.eat = function(someFood) {
+	if (this.stomach.length < 10) {
+		this.stomach.push(someFood);
+	}
+	Person.prototype.poop = function() {
+		this.stomach = [];
+	};
+	Person.prototype.toString = function() {
+		return `${this.name}, ${this.age}`;
+	};
+};
 
 /*
   TASK 2
@@ -57,9 +69,16 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+	this.model = model;
+	this.milesPerGallon = milesPerGallon;
+	this.tank = 0;
+	this.odometer = 0;
 }
+Car.prototype.fill = function(gallons) {
+	this.tank = this.tank + gallons;
+};
+Car.prototype.drive = function(distance) {};
 
 /*
   TASK 3
@@ -68,8 +87,16 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
 
+function Baby(name, age, favoriteToy) {
+	Person.call(this, name, age);
+	this.favoriteToy = favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype);
+{
+	Baby.prototype.play = function() {
+		return `Playing with ${this.favoriteToy}`;
+	};
 }
 
 /* 
@@ -82,14 +109,21 @@ function Baby() {
   4. 
 */
 
-
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
 if (typeof exports !== 'undefined') {
-  module.exports = module.exports || {}
-  if (Airplane) { module.exports.Airplane = Airplane }
-  if (Person) { module.exports.Person = Person }
-  if (Car) { module.exports.Car = Car }
-  if (Baby) { module.exports.Baby = Baby }
+	module.exports = module.exports || {};
+	if (Airplane) {
+		module.exports.Airplane = Airplane;
+	}
+	if (Person) {
+		module.exports.Person = Person;
+	}
+	if (Car) {
+		module.exports.Car = Car;
+	}
+	if (Baby) {
+		module.exports.Baby = Baby;
+	}
 }
